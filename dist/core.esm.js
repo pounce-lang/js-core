dc2c22e645bda1b1363ff6a85e4f7b8a3dd57442
nmorse <morse.nate@gmail.com>
2019-12-14 19:00:47 -0500

    init with klap

---
+++ dist/core.esm.js
@@ -0,0 +1,4 @@
+const sum = (a, b) => a + b;
+
+console.log('this works => ', sum(2, 3));
+//# sourceMappingURL=core.esm.js.map
---
+++ public/index.js
@@ -0,0 +1,3 @@
+import { sum } from '../src/core';
+
+console.log('this works => ', sum(2, 3));
