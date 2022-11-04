727a3d4eab38c206e6cdc6480df79bfa14d1f1a1
Nate Morse <morse.nate@gmail.com>
2022-11-03 21:29:15 -0400

    a single interpreter based on purr, not two!

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -23,6 +23,8 @@ export function* interpreter(
   // preProcess if needed 
   const wd_in = opt.wd ? opt.wd : coreWords;
   let internalCallStack = [];
+  
+  // programList and wordDictionary are preProcessed and parsed (if needed) 
   let [pl, wd] = r.is(Array, pl_in) ? [toPLOrNull(pl_in), wd_in] : preProcessDefs(r.is(String, pl_in) ? parser(pl_in.toString()) : pl_in, wd_in);
 
   // type check ahead
@@ -31,59 +33,17 @@ export function* interpreter(
   let s: ValueStack = [];
   opt?.logLevel ? yield { stack: s, prog: pl, active: true } : null;
   let w;
-  const maxCycles = opt.maxCycles || 1000000;
-  let cycles = 0;
-  while (cycles < maxCycles && internalCallStack.length < 1000
-    && (w = pl.shift()) !== undefined
-    && !(s?.length === 1 && s[0] === null)) {
-    cycles += 1;
-    let wds: WordValue = r.is(String, w) ? wd[w as string] : null;
-    if (wds) {
-      opt.logLevel && !opt.yieldOnId ? debugLevel(internalCallStack, opt.logLevel) ? yield { stack: s, prog: debugCleanPL([w].concat(pl)), active: true, internalCallStack: [...internalCallStack] } : null : null;
-      if (typeof wds.compose === 'function') {
-        let s_ret: ValueStack | null = null;
-        [s, pl = pl] = wds.compose(s, pl);
-        // if(r.isNil(s_ret)) {
-        //   cycles = maxCycles;
-        // }
-        // else {
-        //   s = s_ret;
-        // }
-      }
-      else {
-        if (w === "popInternalCallStack") {
-          internalCallStack.pop();
-        }
-        else {
-          let plist = toPLOrNull(wds.compose);
-          if (plist) {
-            internalCallStack.push(toStringOrNull(w));
-            pl = [...plist, "popInternalCallStack", ...pl];
-          }
-        }
-      }
-    }
-    else if (w !== undefined) {
-      if (r.is(Array, w)) {
-        s?.push([].concat(w));
-      }
-      else {
-        s?.push(w);
-      }
-      opt.logLevel && opt.yieldOnId ? (debugLevel(internalCallStack, opt.logLevel)) ? yield { stack: s, prog: debugCleanPL([w].concat(pl)), active: true, internalCallStack: [...internalCallStack] } : null : null;
-    }
+  const maxCycles = opt.maxCycles || 1;
+  
+  const purrer = purr(pl, wd, maxCycles);
+  const state1 = purrer.next(maxCycles);
+  console.log(state1);
+  while(!purrer.done) {
+    const state = purrer.next(maxCycles);
+    console.log(state.prog);
+    yeild purrer.next(maxCycles);
   }
-  if (s?.length === 1 && s[0] === null) {
-    console.log("s has null");
-    yield { stack: [], prog: pl, active: false, internalCallStack: [...internalCallStack], error: "a word did not find required data on the stack" };
-  }
-  if (cycles >= maxCycles) {
-    yield { stack: s, prog: pl, active: false, internalCallStack: [...internalCallStack], error: "maxCycles size exceeded: this may be an infinite loop" };
-  }
-  if (internalCallStack.length >= 1000) {
-    yield { stack: s, prog: pl, active: false, internalCallStack: [...internalCallStack], error: "callStack size exceeded: this may be an infinite loop" };
-  }
-  return { stack: s, prog: pl, active: false };
+  return purrer.value;
 }
 
 // (more closer to a) production version interpreter
@@ -93,7 +53,7 @@ export function* interpreter(
 export function* purr(
   pl: ProgramList,
   wd: WordDictionary,
-  cycleLimit: number = 100000
+  cycleLimit: number = 10000
 ) {
   let s: ValueStack = [];
   let cycles = 0;
